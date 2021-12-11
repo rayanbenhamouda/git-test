fd0eaa2ee1411af490333ec455f7cb1cb6e09d53
rayanbenhamouda <rayan.ben_hamouda@edu.devinci.fr>
2021-12-11 11:44:08 +0100

    oups erreur

--- ex2.c
+++ ex2.c
@@ -1,21 +1,4 @@
 int main(void) {
   struct list *l = NULL;
-  struct list *l = list_init(1);
-  list_print(l);
-  list_append(l, 2);
-  list_print(l);
-  l = list_prepend(l,42);
-  list_print(l);
-  printf("list length is %zu\n", list_length(l));
-
-  struct list *l2 = list_init(32);
-  list_append(l2, 22);
-  list_append(l2, 53);
-  list_append(l2, 0);
-  list_print(l2);
-  l = list_concat(l, l2);
-  list_print(l);
-  printf("concatened list length is %zu\n", list_length(l));
-  list_destroy(l);
   return 0;
 }
